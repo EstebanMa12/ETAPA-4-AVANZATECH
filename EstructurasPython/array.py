bb1e3fad3a108919fe45ba72f667c3c4e8f6802d
EstebanMa12 <desteban.maya@udea.edu.co>
2023-08-18 12:00:02 -0500

    Starting with singly linked list

--- EstructurasPython/array.py
+++ EstructurasPython/array.py
@@ -1,7 +1,7 @@
 class Array:
     def __init__(self, capacity, fill_value=None):
         self.items = list()
-        for i in range(capacity):
+        for _ in range(capacity):
             self.items.append(fill_value)
         
     def __len__(self):
