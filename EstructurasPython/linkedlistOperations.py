e850818fc2602e12bf5177cf9e1bc5067fe3e71e
EstebanMa12 <desteban.maya@udea.edu.co>
2023-08-19 17:01:25 -0500

    Fixed a bug in the linked list operations code.

--- EstructurasPython/linkedlistOperations.py
+++ EstructurasPython/linkedlistOperations.py
@@ -4,6 +4,6 @@ head = None
 for count in range(1, 6):
     head = Node(count, head)
 
-    while head != None:
-        print(head.data)
-        head = head.next
+while head != None:
+    print(head.data)
+    head = head.next
