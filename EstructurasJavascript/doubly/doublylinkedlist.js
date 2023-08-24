ff20336889442cb9e9c4500d5cd2ea7eb54e70ea
EstebanMa12 <desteban.maya@udea.edu.co>
2023-08-24 11:12:13 -0500

    "Working with a double linked list and his methods"

---
+++ EstructurasJavascript/doubly/doublylinkedlist.js
@@ -0,0 +1,58 @@
+class twoWayNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.previous = null;
+    }
+}
+class MyDoublyLinkedList {
+    constructor(value){
+        this.head={
+            value: value,
+            next: null,
+            previous: null,
+        }
+        this.tail = this.head;
+        this.length = 1;
+    }
+    append(value){
+        const newNode = new twoWayNode(value)
+        newNode.previous = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+    prepend(value){
+        const newNode = new twoWayNode(value)
+        newNode.next = this.head;
+        this.head.previous = newNode;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+    insert(value, position){
+        if(position >= this.length){
+            return this.append(value);
+        }
+        const newNode = new twoWayNode(value);
+        const firstPointer = this.getTheIndex(position - 1);
+        const holdingPointer = firstPointer.next;
+        firstPointer.next = newNode;
+        newNode.previous = firstPointer;
+        newNode.next = holdingPointer;
+        holdingPointer.previous = newNode;
+        this.length++;
+        return this; // return the linked list
+    }
+    getTheIndex(index){
+        let current = this.head;
+        let count = 0;
+        while(count !== index){
+            current = current.next;
+            count++;
+        }
+        return current
+    }
+}
+
