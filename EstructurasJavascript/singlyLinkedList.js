be4ad5fcecf891e65142851adc7e7b5512d95608
EstebanMa12 <desteban.maya@udea.edu.co>
2023-08-24 11:02:46 -0500

    Added 3 new methods to the SinglyLinkedList class: prepend(), insert(), and getTheIndex(). * prepend(): adds a new node to the beginning of the list. * insert(): adds a new node at a specified position in the list. * getTheIndex(): returns the node at a specified position in the list.

--- EstructurasJavascript/singlyLinkedList.js
+++ EstructurasJavascript/singlyLinkedList.js
@@ -22,9 +22,40 @@ class MySinglyLinkedList {
         this.length++;
         return this; // return the linked list
     }
+    prepend(value){
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this; // return the linked list
+    }
+    insert(value, position){
+        if(position >= this.length){
+            return this.append(value);
+        }
+        const newNode = new Node(value);
+        const firstPointer = this.getTheIndex(position - 1);
+        const holdingPointer = firstPointer.next;
+        firstPointer.next = newNode;
+        newNode.next = holdingPointer;
+        this.length++;
+        return this; // return the linked list
+    }
+    getTheIndex(index){
+        let current = this.head;
+        let count = 0;
+        while(count !== index){
+            current = current.next;
+            count++;
+        }
+        return current
+    }
 }
 const myList = new MySinglyLinkedList(1);
 console.log(myList);
-myList.append(2);
 myList.append(3);
+console.log(myList);
+myList.prepend(0);
+console.log(myList);
+myList.insert(1.5, 2);
 console.log(myList);
