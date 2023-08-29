e030a3f71b306d570a67ca1b930e5d2edc382c74
EstebanMa12 <desteban.maya@udea.edu.co>
2023-08-29 16:58:09 -0500

    Add meta binary search function

--- Algoritmos/searchingAlgorithms/metaBinarySearch.py
+++ Algoritmos/searchingAlgorithms/metaBinarySearch.py
@@ -1,2 +1,25 @@
-def meta_binary_search(arrat, target):
-    pass
+import random
+
+def meta_binary_search(array, target):
+    n = len(array)
+    interval_size = n
+
+    while interval_size > 0:
+        index = int(min(n - 1, interval_size / 2))
+        mid_index = (index + interval_size) // 2
+        mid = array[mid_index]
+        
+        if mid == target:
+            return mid_index
+        elif mid < target:
+            interval_size = (interval_size + index) // 2
+        else:
+            interval_size = index // 2
+    
+    return f"No se encontro {target} en la lista"
+if __name__ == "__main__":
+    b = int(input("Ingrese el tamaño de la lista: "))
+    target = int(input("Ingrese el número que desea buscar: "))
+    array = sorted([random.randint(0,100) for _ in range(b)])
+    print(array)
+    print(meta_binary_search(array, target)) # 4
