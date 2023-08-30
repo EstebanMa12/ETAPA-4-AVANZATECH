e08b57a34a90fad8d424328f8405bcc218decf22
EstebanMa12 <desteban.maya@udea.edu.co>
2023-08-30 15:21:31 -0500

    End of Ternary search algorithm

--- Algoritmos/searchingAlgorithms/ternarySearch.py
+++ Algoritmos/searchingAlgorithms/ternarySearch.py
@@ -29,6 +29,25 @@ def ternary_search(low=0, high=None, target=0, array=[]):
     return -1, 0 
 
 def ternary_search_iterative(low=0, high=None, target=0, array=[]):
+    if high is None:
+        high = len(array) - 1 
+    while low <= high:
+        mid1 = low + (high - low)//3
+        mid2 = high - (high - low)//3
+
+        if array[mid1]==target:
+            return mid1, 1
+        elif array[mid2]==target:
+            return mid2, 1
+        elif target < array[mid1]:
+            high = mid1 -1
+        elif target > array[mid2]:
+            low  = mid2 + 1
+        else:
+            low = mid1 + 1
+            high = mid2 - 1
+    return -1
+
     pass
 if __name__ == "__main__":
     b = int(input("Ingrese el tamaño de la lista: "))
@@ -36,3 +55,5 @@ if __name__ == "__main__":
     array = sorted([random.randint(0,100) for _ in range(b)])
     print(array)
     print(ternary_search(target=target, array=array)) # 4
+    print(ternary_search_iterative(target=target, array=array)) # 4
+
