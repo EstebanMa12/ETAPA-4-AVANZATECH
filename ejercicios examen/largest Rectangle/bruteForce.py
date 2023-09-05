83fd17c4d692fdf0d6fd30ae58bc1dc02f9c5687
EstebanMa12 <desteban.maya@udea.edu.co>
2023-09-05 15:41:59 -0500

    Changed the way minHeight is calculated. Now it is the minimum between the current minHeight and the current array element// The complexity now is n^2

--- ejercicios examen/largest Rectangle/bruteForce.py
+++ ejercicios examen/largest Rectangle/bruteForce.py
@@ -2,10 +2,11 @@ def bruteForceAlgorithm(array):
   n = len(array)
   maxArea = 0 
   for i in range(n):
+    minHeight=float('inf')
     for j in range(i,n):
-      minHeight = min(array[i:j+1])
+      minHeight = min(minHeight,array[j])
       maxArea = max(maxArea,(j-i+1)*minHeight)
-      print(f"min: {minHeight}; max :{maxArea}")
+    print(f"min: {minHeight}; max :{maxArea},array: {array[i]}")
   return maxArea
 
 if __name__=="__main__":
